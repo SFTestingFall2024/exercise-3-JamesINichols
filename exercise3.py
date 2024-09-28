fced610fa3cc764aff2d9ce07344a9ca81d62782
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 19:40:41 -0400

    Diego B

--- exercise3.py
+++ exercise3.py
@@ -1,43 +1,30 @@
-'''
-Author: Bruno Carballo
-Course ID and Section: COP2002.0T2
-Date: 09/22/2024
+#Diego Baez
+#COP2002.0T2
+#James Nichols
 
-Program Title: MAC Manufacturer Program
+#Create program welcome message
+print("           MAC Manufacturer Program")
+print("------------------------\n")
 
-Program Description: This program prompts the user to enter the first six digits
-                     of a hex code and the program will return the name of the 
-                     manufacturer associated with said hex code.
-    
-'''
-#Establishing the lists that will hold the hex codes and manufacturers
-hex_digits=["00:00:17","00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
-manufacturers=["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", 
-               "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc", "Unknown"]
 
-#main function in which the program runs
-def main():
+#Create a variable for the user to input a hex value
+hexCode = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
-#Title Message and user prompt
-    print("MAC Manufacturer Program\n------------------------")
-    hex_input = input("\n\nEnter the first 6 hex values of the MAC address (format as xx:xx:xx): ")
+#create an if elif else structure that assigns the variable corporation with a hex value
+if (hexCode=="00:00:17"):
+    corporation="Oracle"
+elif (hexCode=="00:07:E9"):
+    corporation="Intel Corporation"
+elif (hexCode=="04:27:28"):
+    corporation="Microsoft Corporation"
+elif (hexCode=="04:26:65"):
+    corporation="Apple, Inc."
+elif (hexCode=="04:33:89"):
+    corporation="Huawei Technologies Co.,Ltd"
+elif (hexCode=="00:00:0C"):
+    corporation="Cisco Systems, Inc"
+else:
+    corporation="Unknown"
 
-#If/elif/else statements that will go through each possible outcome and will print the proper response
-    if hex_input == hex_digits[0]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[0] + ".")
-    elif hex_input == hex_digits[1]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[1] + ".")
-    elif hex_input == hex_digits[2]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[2] + ".")
-    elif hex_input == hex_digits[3]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[3] + ".")
-    elif hex_input == hex_digits[4]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[4] + ".")
-    elif hex_input == hex_digits[5]:
-        print("For " + hex_input + " the MAC manufacturer is " + manufacturers[5] + ".")
-    else:
-        print('For "INVALID MAC" the MAC manufacturer is ' + manufacturers[6] + ".")
-
-#If statement to call the main fucntion
-if __name__ == "__main__":
-    main()
+#Create the print statement that displays the outcome
+print(f"For {hexCode} the MAC manufacturer is {corporation}.")
