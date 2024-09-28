a5961dd13b19bb3cf2d7642d1c6fd0bbcd59741a
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 14:28:34 -0400

    Tong

--- exercise3.py
+++ exercise3.py
@@ -1,49 +1,45 @@
-#!/usr/bin/python3.12
+# Tong Fu(TongFu928)
+# COP2002-0T2
+# September 20, 2024
+# MAC Manufacturer Program
+# Input the first 6 hex digits and provides the manufacturer
 
-# David Holbrook (mriservice)
-# COP2002-0T1
-# September 22, 2024
-# Exercise 3: If Statements 
-# Create a program that asks the user for the first 6 hex digits of a NIC
-# formatted as XX:XX:XX and provides the manufacturer
+def main():
 
-# Construct Hardware Data
-nic_cards = [
-    ["00:00:17","Oracle"],
-    ["00:07:E9","Intel Corporation"],
-    ["04:27:28","Microsoft Corporation"],
-    ["04:26:65","Apple, Inc."],
-    ["04:33:89","Huawei Technologies Co.,Ltd"],
-    ["00:00:0C","Cisco Systems, Inc"]
-    ]
+    # Display program title and format
+    print("MAC Manufacturer Program")
+    print("------------------------")
+    
+    # list of known
+    manufacturers = [
+        ["00:00:17", "Oracle"],
+        ["00:07:E9", "Intel Corporation"],
+        ["04:27:28", "Microsoft Corporation"],
+        ["04:26:65", "Apple, Inc."],
+        ["04:33:89", "Huawei Technologies Co.,Ltd"],
+        ["00:00:0C", "Cisco Systems, Inc"]
+        ]
 
-# Construct Header 
-header = "MAC Manufacturer Program\n"
-header += "------------------------\n\n"
+    # Input the the first 6 hex values
+    userInput = input("\nEnter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
-input_header = "Enter the first 6 hex values of the Mac address (format as XX:XX:XX): "     # Construct user input Header
+    # Default value
+    manufacturer = "Unknown"
 
-user_input = ""                                                                             # user input variable
+    # Loop through the list, each list item is called an set
+    for set in manufacturers:
+        adress = set[0]
+        name = set[1]
 
-device_found = False                                                                        # Found variable Boolean Flag
+        if userInput == adress:
+            manufacturer = name
 
-print(header)                                                                               # Display Header
+    # output the result
+    if manufacturer == "Unknown":
+        print(f"For {userInput} the MAC manufacturer is Unknown.")
 
-# Get user input
-while (user_input==""):
-
-    user_input = input(input_header)
-
-# Check Hardware List for match and display output
-for card in nic_cards:
-
-    if card[0] == user_input:
-
-        print(f"For {card[0]} the MAC manufacturer is {card[1]}.")
-
-        device_found = True
-
-# Check Hardware not found
-if not device_found:
-
-    print(f"Not valid value or not found: {user_input}")
+    else:
+        print(f"For {userInput} the MAC manufacturer is {manufacturer}.")
+            
+if(__name__=="__main__"):
+    main()
