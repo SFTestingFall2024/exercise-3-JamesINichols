1adc980c9293c6fa174993d60f40238c32f84f4c
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 19:22:02 -0400

    Walter S

--- exercise3.py
+++ exercise3.py
@@ -1,47 +1,36 @@
-# Stephen MacDonald
-# COP2002-OT2
-# Sept. 20, 2024
-# Exercise 2: If Statements
-# This program asks the user for 6 hex values (formatted as XX:XX:XX)
-# and then returns the manufacturer name for the hex value given
+#Wjschratt, Walter Schratt
+#COP2002
+#09/20/24
+#excersice3
+#identifies manufacturer based on user input 
+
 
 def main():
+    print("User Manufacturer Program \n------------------------- \n")
+    mac = input("Please enter the first 6 hex digits of the MAC address formatted as XX:XX:XX: ")
 
-# MAC Output Statement
+    ORACLE = "00:00:17"
+    INTEL = "00:07:E9"  
+    MICROSOFT = "04:27:28"
+    APPLE = "04:26:65"
+    HUAWEI = "04:33:89"  
+    CISCO = "00:00:0C"
 
-    print("MAC Manufacturer Program")
-
-# Dashes Output Statement
-
-    print("------------------------");
-
-# Blank Line Statement
-
-    print("\n")
-
-# User Prompt
-
-    address=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ");
-
-# If statement to print the manufacturer of the given MAC address
-
-    if address=="00:00:17":
-        print("For 00:00:17 the MAC manufacturer is Oracle")
-    elif address=="00:07:E9":
-        print("For 00:07:E9 the MAC manufacturer is Intel Corporation")
-    elif address=="04:27:28":
-        print("For 04:27:28 the MAC manufacturer is Microsoft Corporation")
-    elif address=="04:26:65":
-        print("For 04:26:65 the MAC manufacturer is Apple, Inc.")
-    elif address=="04:33:89":
-        print("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd")
-    elif address=="00:00:0C":
-        print("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc")
+    if mac == ORACLE:
+        print("For " + mac + " the MAC manufacturer is Oracle")
+    elif mac == INTEL:
+        print("For " + mac + " the MAC manufacturer is Intel")
+    elif mac == MICROSOFT:
+        print("For " + mac + " the MAC manufacturer is Microsoft")
+    elif mac == APPLE:
+        print("For " + mac + " the MAC manufacturer is Apple")
+    elif mac == HUAWEI:
+        print("For " + mac + " the MAC manufacturer is Huawei")  
+    elif mac == CISCO:
+        print("For " + mac + " the MAC manufacturer is Cisco")
     else:
-        print("The manufacturer for this MAC address is unknown.")
-    
-    
+        print("Value not found")  
 
 
-if(__name__=="__main__"):
+if __name__ == "__main__":
     main()
