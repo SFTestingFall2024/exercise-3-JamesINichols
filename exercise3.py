9856d15e3d02b238eabd1df7b6f41439b68c5a1b
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 20:40:31 -0400

    Logan

--- exercise3.py
+++ exercise3.py
@@ -1,47 +1,37 @@
-# Sangki Jun
-# COP2002 0T2 
-# 9/19/24
-# Exercise 2
-# Prompt the user to enter a MAC address in XX:XX:XX format, then provide the manufacturer of that address
+# Logan Proferes [brackets are dev notes]
 
-# main function
-def main():
-    # prints output statements
-    print("MAC Manufacturer program")
-    print("------------------------")
-    print()
-    
-    # prompt the user to enter the 6 hex digits 
-    hexDigit = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+# COP2002 F24
 
-    # if statement to check entered hex digit and output correct manufacturer
-    if (hexDigit == "00:00:17"):
-        print(f"For {hexDigit} the MAC manufacturer is Oracle.")
+# 9/26/24
 
-    # elif statement to check entered hex digit and output correct manufacturer
-    elif (hexDigit == "00:07:E9"):
-        print(f"For {hexDigit} the MAC manufacturer is Intel Corporation.")
-            
-    # elif statement to check entered hex digit and output correct manufacturer
-    elif (hexDigit == "04:27:28"):
-        print(f"For {hexDigit} the MAC manufacturer is Microsoft Corporation.")
+#MAC/NIC adress finder program (or exercise3.py)
 
-    # elif statement to check entered hex digit and output correct manufacturer
-    elif (hexDigit == "04:26:65"):
-        print(f"For {hexDigit} the MAC manufacturer is Apple, Inc.")
+# Desc: Finds the NIC/MAC adress [It's a little unclear the top description says NIC but the example image says MAC so idk.] and then looks through
+# the set values to find a list.
 
-    # elif statement to check entered hex digit and output correct manufacturer
-    elif (hexDigit == "04:33:89"):
-        print(f"For {hexDigit} the MAC manufacturer is Huawei Technologies Co.,Ltd.")
 
-    # elif statement to check entered hex digit and output correct manufacturer
-    elif (hexDigit == "00:00:0C"):
-        print(f"For {hexDigit} the MAC manufacturer is Cisco Systems, Inc.")
 
-    # if input is not one of MAC address listed above, output manufacturer is unknown
+
+# Input statement to get the code for the manufacturers.
+
+NIC_Manufacture_Code = str(input("Please insert the first 6 digits of your NIC manufacurer code here. \n(Format as XX:XX:XX):  "))
+
+#Two lists to check the values from the input margin.
+
+Codes_Base = ["00:00:17","00:07:E9","04:27:65","04:26:65","04:33:89","00:00:0C" ]
+Manufacturers = ["Oracle","Intel Corp.","Microsoft Corperation","Apple, Inc.","Huawei Technologiges Co., ltd","Cisco Systems, Inc.", "Unknown."]
+
+#This loop runs through the upper two arrays, which compares that values in there to the input. This loops to check each list entry for a match.
+#If it can't find a match, it runs the else code as a failsafe. i is used as a basic variable to force an increse in the loop if it doesn't find anything.
+
+i = 0
+while (i < len(Codes_Base)):
+    if (NIC_Manufacture_Code == Codes_Base[i]):
+     print (NIC_Manufacture_Code + " is a NIC from " + Manufacturers[i]+".")
+     break
     else:
-        print(f"For {hexDigit} the MAC manufacturer is unknown.")
-
-# if statment that calls main function to run program       
-if (__name__ == "__main__"):
-    main()
+     i += 1
+else:
+ print(NIC_Manufacture_Code + " is a NIC from " + Manufacturers[-1]+".")
+ 
+ 
