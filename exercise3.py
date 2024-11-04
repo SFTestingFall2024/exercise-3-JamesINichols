43e46b1301014d0521feed1f37af7fd84000028c
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-04 09:03:20 -0500

    Robert C

--- exercise3.py
+++ exercise3.py
@@ -1,37 +1,62 @@
-# Logan Proferes [brackets are dev notes]
+lista_Card=[] #This list  have The ID each NIC
+message=""
+formato1=""
+formato2=""
+formato3=""
+formato4=""
+formato5=""
+formato6=""
+while message.lower()!="q": # Controla el programa en general
+     message= input("Enter Q to quit or Continue to begin the program:  ")# Se debe tipear cualquier palabra para continua o Q para salir
+     nic_Card=input("Introduce The first 6 hex digits in formatted XX:XX:XX  ") # This will be list that input the Users
+     if len(nic_Card)==8 and (nic_Card[2]==":")and (nic_Card[5]==":"):
+      lista_Card=["0","0",":","0","0",":","1","7"]
+     if list(nic_Card)==lista_Card:
+           print("El fabricante es ORACLE")
+           print("Lista Oracle:", lista_Card)
+           print(nic_Card)
+           formato6=f"{nic_Card}\t Oracle "
+           print(formato6)
+     else:
+        lista_Card=["0","0",":","0","7", ":","E","9"]
+        if lista_Card==list(nic_Card):
+           print("Manufacture is INTEL Corporation ")
+           print("List Intel Corporation:",lista_Card)
+           print(nic_Card)
+           formato5=f"{nic_Card}\t Intel Corporation "
+           print(formato5)
+        elif list(nic_Card)==["0","4",":","2","7",":","2","8"]:
+             lista_Card=["0","4",":","2","7",":","2","8"]
+             print("The Manufacture is: Microsof Corporation ", lista_Card)
+             print(nic_Card)
+             formato4=f"{nic_Card}\t Microsof Corporation "
+             print(formato4)
+        elif list(nic_Card)==["0","4",":","2","6",":","6","5"]:    
+             lista_Card=["0","4",":","2","6",":","6","5"]
+             print("The Manufacture is Apple Inc ", lista_Card)
+             print(nic_Card)
+             formato3=f"{nic_Card}\t Apple Inc "
+             print(formato3)
+        elif list(nic_Card)==["0","4",":","3","3",":","8","9"]:
+             lista_Card=["0","4",":","3","3",":","8","9"]   
+             print("The Manufactures is Huawei Techonology Co, Ltd", lista_Card)
+             print(nic_Card)
+             formato2=f"{nic_Card}\t Huawei Technology "
+             print(formato2)
+        elif list(nic_Card)==["0","0",":","0","0",":","0","C"]:
+             lista_Card=["0","0",":","0","0",":","0","C"]   
+             print("The Manufacture is Cisco System, Inc", lista_Card) 
+             print(nic_Card)
+             formato1=f"{nic_Card}\t Cisco Systems, Inc"
+             print(formato1)
+              
+        else:
+          print("This ID not is a valid format Unknown") #THE FORMATT IS DIFFERENT XX:XX:XX
+print(nic_Card)
+formato_matriz = f"{formato1}\n{formato2}\n{formato3}\n{formato4}\n{formato5}\n{formato6}"
+print(formato_matriz)
+print("You entered :"+ message)
 
-# COP2002 F24
-
-# 9/26/24
-
-#MAC/NIC adress finder program (or exercise3.py)
-
-# Desc: Finds the NIC/MAC adress [It's a little unclear the top description says NIC but the example image says MAC so idk.] and then looks through
-# the set values to find a list.
-
-
-
-
-# Input statement to get the code for the manufacturers.
-
-NIC_Manufacture_Code = str(input("Please insert the first 6 digits of your NIC manufacurer code here. \n(Format as XX:XX:XX):  "))
-
-#Two lists to check the values from the input margin.
-
-Codes_Base = ["00:00:17","00:07:E9","04:27:65","04:26:65","04:33:89","00:00:0C" ]
-Manufacturers = ["Oracle","Intel Corp.","Microsoft Corperation","Apple, Inc.","Huawei Technologiges Co., ltd","Cisco Systems, Inc.", "Unknown."]
-
-#This loop runs through the upper two arrays, which compares that values in there to the input. This loops to check each list entry for a match.
-#If it can't find a match, it runs the else code as a failsafe. i is used as a basic variable to force an increse in the loop if it doesn't find anything.
-
-i = 0
-while (i < len(Codes_Base)):
-    if (NIC_Manufacture_Code == Codes_Base[i]):
-     print (NIC_Manufacture_Code + " is a NIC from " + Manufacturers[i]+".")
-     break
-    else:
-     i += 1
-else:
- print(NIC_Manufacture_Code + " is a NIC from " + Manufacturers[-1]+".")
  
+
  
