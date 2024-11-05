d07583ba127942e16c0fa39eb19774da3e4245a9
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 10:37:51 -0500

    Gabe

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,57 @@
-#("Name = Eric caison")
-
-#("Course ID and section = COP2002.OT2")
-
-#("Date = Octomber 9, 2024")
-
-#("program title = Loops")
-
-#("program Description = Manufacturer is for a NIC card")
- 
+# Gabe Bichoneau
+# COP2002-0T1
+# Oct. 23, 2024
+# Exercise 3 If statements
+# Program determines the manufacturer for NIC based on user input. 
 
 
-manufacturer = ["Oracle", "Intel Corporation", "Microsoft Corperation", "Apple, Inc", "Huawei Technologies","Cisco Systems, Inc"]
-mac_address = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
+def main():
+    while True:  # Starts an infinite loop
+        # Print the program title
+        print("\nMAC Manufacturer Program")
+        print("---------------------------------")
 
+        # Prompts user to enter the first 6 hex values of the MAC address
+        hex_digits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
+        # Validate the input format; continue prompting until the format is correct
+        while len(hex_digits) != 8 or hex_digits[2] != ':' or hex_digits[5] != ':':
+            hex_digits = input("\nInvalid format. Please enter in the format XX:XX:XX: ")
 
-found = True
-hex_digits = input("Enter hex digits:")
-for i in range(len(mac_address)):
-    if hex_digits == mac_address[i]:
-        print((manufacturer[i]))
-        found = True
-        if hex_digits != mac_address[i + 1]:
-            break
-    else:
-        found = False
-        
-if found == False:
-    print("Mac address doesnt match up")
+        # List of known manufacturers with their corresponding MAC address prefixes
+        manufacturers = [
+            ("00:00:17", "Oracle"),
+            ("00:07:E9", "Intel Corporation"),
+            ("04:27:28", "Microsoft Corporation"),
+            ("04:26:65", "Apple, Inc."),
+            ("04:33:89", "Huawei Technologies Co., Ltd"),
+            ("00:00:0C", "Cisco Systems, Inc")
+        ]
+
+        # Default message for unknown manufacturers
+        manufacturer = "not a valid value or is not found"
+
+        # Loop through the manufacturer list to find a match for the provided hex digits
+        for code, name in manufacturers:
+            if hex_digits == code:
+                manufacturer = name  # Update manufacturer if a match is found
+                break  # Exit the loop once the manufacturer is found
+            
+   
+        # Print the result showing the manufacturer associated with the MAC address
+        print(f"\n For {hex_digits}, the MAC manufacturer is {manufacturer}")
+      
+
+        # Ask the user if they want to continue or exit
+        while True:  # Keep prompting until a valid response is given
+            exit_prompt = input("\nPress 'y' to enter another MAC address, or 'n' to exit: ")
+            if exit_prompt.lower() == 'y':
+                break  # Exit the inner loop to re-prompt for the MAC address
+            elif exit_prompt.lower() == 'n':
+                print("\nExiting the program. Goodbye!")
+                return  # Exit the program
+            else:
+                print("\nInvalid option. Please enter 'y' or 'n'.")
+
+if __name__ == "__main__":
+    main()  # Call the main function to run the program
