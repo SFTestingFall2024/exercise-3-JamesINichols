f7e823a54f659577dabbfe2417132c17298cd23a
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 09:18:01 -0500

    Juan

--- exercise3.py
+++ exercise3.py
@@ -1,41 +1,22 @@
-# Tyler McWatters
-# COP2002.0T2
-# Oct 5th, 2024
-# Exercise 3
-# If statements
+# Part 1 Exercise 3, Used lists to define hex digits and manufacturers
+print("MAC Manufacturer Program")
+print("-" * 24)
+hex_digits =["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C"]
+manufacturers =["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
 
-# defining MAC manufactureres
-def getMacManufacturer(hexValue):
+# Define user input and organize by line
+user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
 
-    # MAC manufacturers
-    macAddress = {
-        "00:00:0C": "Cisco Systems, Inc",
-        "00:00:17": "Oracle",
-        "00:07:E9": "Intel Corporation",
-        "04:27:28": "Microsoft Corporation",
-        "04:26:65": "Apple, Inc",
-        "04:33:89": "Huawei Technologies Co., Ltd"
-    }
+print("-" * 24)
 
-    return macAddress.get(hexValue.upper(), "Unknown")
+# Start of if/else Statement
+if user_input in hex_digits:
+    index = hex_digits.index(user_input)
+    result_manufacturer = manufacturers[index]
+else:
+    result_manufacturer= "unknown"
+# Print Results based on hexadecimal user input
+print(f"Hex Digits: {user_input}\nManufacturer: {result_manufacturer}")
+print("-" * 24)
 
-# Main
-def main():
 
-    # Title display
-    title = "MAC Manufacturer Program"
-    print(title)
-    print('-' * len(title))
-    print("\n")
-      
-    hexValue = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    
-    # Print MAC Manufacturers
-    manufacturer = getMacManufacturer(hexValue)
-    print(f"For {hexValue} the MAC manufacturer is {manufacturer}.\n")
-
-    print("\nThe program has ended.")
-
-# Calling Main Function
-if __name__ == "__main__":
-    main()
