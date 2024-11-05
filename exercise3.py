235590a276494efc7f8f06115d6779a3fd37a532
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 10:11:49 -0500

    Shema

--- exercise3.py
+++ exercise3.py
@@ -1,27 +1,42 @@
-#Kareem Dagher (Torch2March)
-#COP2002-0T2
-#Oct 16,2024
-#MAC Manufacturer program
-#Program identifies manufacturer
+#Shemab1273
+COP2002_0T2
+10/16/2024
+If_Statement
+Create a program to determaine the manufacturer of some NIC card 
 
-Manufacturer = ["Oracle", "Intel corporation", "Microsoft Corporation", "Apple, Inc", "Huawei Technologies Co.,Ltd", "Cisco systems, Inc", "Unknown"]
-HexDigits = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
+
+
+
+print('MAC Manufacturer Program')         #Title of program
+print('-------------------------')
+print()
+
+
+#input   
+manufacturer =["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc", "Huawei Tech Co., Ltd", "Cisco Sys, Inc"]   #variables
+hex_digits = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]    #values
+
 
 def main():
 
-    digits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+#input statement that 6 hex values need to be entered
+   user_input = input("Enter the first 6 hex values of the MAC address (format as xx:xx:xx): ")    
+
+#use the lenght of hex_digits to identify the index
+   for i in range(len(hex_digits)):
+       if hex_digits[i] == user_input: 
+           print(f"For {user_input} the MAC manufacturer is {manufacturer[i]}.")
+           break
+   else:
+      print(f"For {user_input} the MAC manufacturer is {"Unknown"}.") 
+
+
+
+
     
-    if(digits == HexDigits[0]): print("for " + HexDigits[0] + " the MAC manufacturer is " + Manufacturer[0])
-    elif(digits == HexDigits[1]): print("for " + HexDigits[1] + " the MAC manufacturer is " + Manufacturer[1])
-    elif(digits == HexDigits[2]): print("for " + HexDigits[2] + " the MAC manufacturer is " + Manufacturer[2])
-    elif(digits == HexDigits[3]): print("for " + HexDigits[3] + " the MAC manufacturer is " + Manufacturer[3])
-    elif(digits == HexDigits[4]): print("for " + HexDigits[4] + " the MAC manufacturer is " + Manufacturer[4])
-    elif(digits == HexDigits[5]): print("for " + HexDigits[5] + " the MAC manufacturer is " + Manufacturer[5])
-    else: print(Manufacturer[6])
-    print("\n")
 
-def start():
-    print("MAC manufacturer program" + "\n------------------------\n")
+   
+
+
+if(__name__=="__main__"):
     main()
-
-start()
