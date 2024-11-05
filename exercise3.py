eda8120b046c0983efc15ce978df61432bd6c74e
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 10:31:06 -0500

    Eric C

--- exercise3.py
+++ exercise3.py
@@ -15,22 +15,16 @@ mac_address = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:0
 
 
 
-while True:
-    found = True
-    hex_digits = input("Enter hex digits:")
-    if hex_digits.lower() == 'exit':
-        break
-    for i in range(len(mac_address)):
-        if hex_digits == mac_address[i]:
-            print((manufacturer[i]))
-            found = True
-            if hex_digits != mac_address[i + 1]:
-                break
-        else:
-            found = False
-            
-    if found == False:
-        print("Mac address doesnt match up")
-
-if __name__ == "__main__":
-    main()
+found = True
+hex_digits = input("Enter hex digits:")
+for i in range(len(mac_address)):
+    if hex_digits == mac_address[i]:
+        print((manufacturer[i]))
+        found = True
+        if hex_digits != mac_address[i + 1]:
+            break
+    else:
+        found = False
+        
+if found == False:
+    print("Mac address doesnt match up")
