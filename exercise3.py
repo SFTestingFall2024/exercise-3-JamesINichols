3c9a40787ee0518f8300ef3db2081b46dc0c7a2f
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 09:28:23 -0500

    Lexi

--- exercise3.py
+++ exercise3.py
@@ -1,22 +1,39 @@
-# Part 1 Exercise 3, Used lists to define hex digits and manufacturers
-print("MAC Manufacturer Program")
-print("-" * 24)
-hex_digits =["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C"]
-manufacturers =["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
+# Define a dictionary to map the first 6 hex digits to the corresponding manufacturers
+mac_manufacturers = {
+    "00:00:17": "Oracle",
+    "00:07:E9": "Intel Corporation",
+    "04:27:28": "Microsoft Corporation",
+    "04:26:65": "Apple, Inc.",
+    "04:33:89": "Huawei Technologies Co.,Ltd",
+    "00:00:0C": "Cisco Systems, Inc",
+}
 
-# Define user input and organize by line
-user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
+# Define a function to look up the manufacturer by the given hex digits
+def get_manufacturer(hex_input):
+    hex_values = hex_input.split(":")
+    if len(hex_values) == 3:
+        manufacturer_key = ":".join(hex_values).lower()
+        if manufacturer_key in mac_manufacturers:
+            return mac_manufacturers[manufacturer_key]
+        else:
+            return "Unknown"
+    else:
+        return "Invalid format. Please use the correct format: XX:XX:XX"
 
-print("-" * 24)
+# Main program to interact with the user
+def main():
+    print("""
+           MAC Manufacturer Program
+------------------------
+""")
+    while True:
+        hex_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+        manufacturer = get_manufacturer(hex_input)
+        if manufacturer == "Invalid format. Please use the correct format: XX:XX:XX":
+            continue
+        print(f"For {hex_input} the MAC manufacturer is {manufacturer}.")
+        break
 
-# Start of if/else Statement
-if user_input in hex_digits:
-    index = hex_digits.index(user_input)
-    result_manufacturer = manufacturers[index]
-else:
-    result_manufacturer= "unknown"
-# Print Results based on hexadecimal user input
-print(f"Hex Digits: {user_input}\nManufacturer: {result_manufacturer}")
-print("-" * 24)
-
-
+# Execute the main function
+if __name__ == "__main__":
+    main()
