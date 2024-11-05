acf9460f4d8873c2566475dbbba7342deba7df17
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 09:57:44 -0500

    Kareem

--- exercise3.py
+++ exercise3.py
@@ -1,37 +1,28 @@
-# Logan Proferes (logiepro)[brackets are dev notes]
+#Kareem Dagher (Torch2March)
+#COP2002-0T2
+#Oct 16,2024
+#MAC Manufacturer program
+#Program identifies manufacturer
 
-# COP2002.OT2 F24
+Manufacturer = ["Oracle", "Intel corporation", "Microsoft Corporation", "Apple, Inc", "Huawei Technologies Co.,Ltd", "Cisco systems, Inc", "Unknown"]
+HexDigits = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
 
-# 9/26/24 [Updated 10/15/24]
+def main():
 
-#MAC/NIC address(or exercise3.py)
+    digits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    
+    if(digits == HexDigits[0]): print("for " + HexDigits[0] + " the MAC manufacturer is " + Manufacturer[0])
+    elif(digits == HexDigits[1]): print("for " + HexDigits[1] + " the MAC manufacturer is " + Manufacturer[1])
+    elif(digits == HexDigits[2]): print("for " + HexDigits[2] + " the MAC manufacturer is " + Manufacturer[2])
+    elif(digits == HexDigits[3]): print("for " + HexDigits[3] + " the MAC manufacturer is " + Manufacturer[3])
+    elif(digits == HexDigits[4]): print("for " + HexDigits[4] + " the MAC manufacturer is " + Manufacturer[4])
+    elif(digits == HexDigits[5]): print("for " + HexDigits[5] + " the MAC manufacturer is " + Manufacturer[5])
+    else: print(Manufacturer[6])
+    print("\n")
+    main()
 
-# Desc: Checks the inputed code for matches within the Manufacturers list and spits out something. 
+def start():
+    print("MAC manufacturer program" + "\n------------------------\n")
+    main()
 
-
-
-# Input statement to get the code for the manufacturers.
-print("MAC Manufactuer Program \n----------------------- \n")
-
-MACManufacturer = str(input("Enter the first 6 digits of your MAC adress. \n(Format as XX:XX:XX):  "))
-
-# Two lists to check the values from the input margin.
-# First list is the codes and the second is the manufacturers.
-# They're ordered so that codes share the same index as the company that made them. With the extra in manufacturers being a falesafe if there's no match.
-
-Codes_Base = ["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C" ]
-Manufacturers = ["Oracle","Intel Corporation","Microsoft Corperation","Apple, Inc.","Huawei Technologiges Co., ltd","Cisco Systems, Inc.", "Unknown."]
-
-# This loop runs through the upper two arrays, which compares that values in there to the input. This loops to check each list entry for a match.
-# If it can't find a match, it runs the else code as a failsafe. i is used as a basic variable to force an increse in the loop if it doesn't find anything.
-
-i = 0
-while (i < len(Codes_Base)):
-    if (MACManufacturer == Codes_Base[i]):
-        print ("For " + MACManufacturer + " the MAC manufacturer is " + Manufacturers[i]+".")
-        break
-    else:
-     i += 1
-else:
- print ("For " + MACManufacturer + " the MAC manufacturer is " + Manufacturers[-1]+".")
- 
+start()
